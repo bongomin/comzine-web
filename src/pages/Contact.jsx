b7c601cbcbc596423bbe793b6c74d5539df969c6
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-11 11:57:34 +0300

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Breadcumb from '../components/Breadcumb'
+import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <Breadcumb label="Contact us"/>
+      <ContactForm/>
+      <Footer/>
+    </div>
   )
 }
