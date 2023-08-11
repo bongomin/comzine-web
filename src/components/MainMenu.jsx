f796a27e39be7046b0d90f5d3eba4b7d09de4f66
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-11 08:59:30 +0300

    Use react-router-dom

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const MainMenu = () => {
     return (
         <div className="col-auto">
             <nav className="main-menu menu-style1 d-none d-lg-block">
                 <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About Us</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
                     <li className="menu-item-has-children">
-                        <a href="service.html">Service</a>
+                        <Link to="/service">Service</Link>
                         <ul className="sub-menu">
-                            <li><a href="service.html">Service1</a></li>
-                            <li><a href="service-details.html">Service2</a></li>
+                            <li><Link to="/service">Service1</Link></li>
+                            <li><Link to="/service-details">Service2</Link></li>
                         </ul>
                     </li>
-                    <li><a href="about.html">Contact</a></li>
-                    <li><a href="about.html">Projects</a></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
                 </ul>
                 <button className="vs-menu-toggle d-inline-block d-lg-none">
                     <i className="fal fa-bars"></i>
