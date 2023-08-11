b8c27bb453ac873013f8661bb88dba25fb545ba2
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-11 10:51:35 +0300

    Create routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and Routes
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AboutPage from './pages/About';
+import ContactPage from './pages/Contact';
+import ServicesPage from './pages/Services';
+import ProjectsPage from './pages/Projects';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes> 
+        <Route path="/about" element={<AboutPage />} /> 
+        <Route path="/contact" element={<ContactPage />} /> 
+        <Route path="/services" element={<ServicesPage />} /> 
+        <Route path="/projects" element={<ProjectsPage />} /> 
+
+
+
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
