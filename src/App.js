043582ad38cd13b39889ec65af66534cfcefcdbf
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-15 16:04:53 +0300

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import HeaderTop from './components/HeaderTop';
-import MenuTop from './components/MenuTop';
-import MainMenu from './components/MainMenu';
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Services from './components/Services';
