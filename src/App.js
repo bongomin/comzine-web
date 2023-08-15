e6d5a6e7a77742a28423ea85853093aa63b58a81
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-15 13:27:28 +0300

    code refactor

--- src/App.js
+++ src/App.js
@@ -9,25 +9,12 @@ import CounterSection from './components/CounterSection';
 import FeaturesAndAboutSection from './components/FeaturesAndAboutSection';
 import ImageCarousel from './components/ImageCarousel';
 import Team from './components/Team';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="vs-header header-layout1">
-        <HeaderTop />
-        <div className="container">
-          <MenuTop />
-        </div>
-        <div className="sticky-wrapper">
-          <div className="sticky-active">
-            <div className="container">
-              <div className="row align-items-center justify-content-between">
-                <MainMenu />
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header/>
       <HeroSection />
       <ImageCarousel/>
       <FeaturesAndAboutSection />
